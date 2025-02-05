ad595dde5d7c10c3e9250bb091614ae86605bf79
unknown <lucassousa71@hotmail.com>
2025-02-05 11:02:51 -0300

    Refactor e responsivo

--- src/app/favorite/page.tsx
+++ src/app/favorite/page.tsx
@@ -8,7 +8,7 @@ const FavoritePage = () => {
   return (
     <>
       <h1 className="text-2xl text-center w-100 mt-8 mb-8 ">Favorite List</h1>
-      {favoriteList.length > 0 ? (
+      {favoriteList?.length > 0 ? (
         <div className="md:grid md:grid-cols-3 md:gap-8 mb-8 flex flex-wrap text-center">
           {favoriteList.map((item, index) => (
             <FavCardComponent key={index} eventShow={item}></FavCardComponent>
